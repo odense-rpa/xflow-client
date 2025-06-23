e90c87db1410c67f8219cc8070b8d5d9dbda94e0
odensesimof <simof@odense.dk>
2025-06-23 12:10:33 +0200

    Fix: merge typo

--- xflow_client/__init__.py
+++ xflow_client/__init__.py
@@ -3,4 +3,4 @@ from .process_client import ProcessClient
 from .process_template_client import ProcessTemplateClient
 from .value_list_client import ValueListClient
 
-__all__ = ['XFlowClient', 'ProcessClient', 'ValueListClient', 'ProcessTemplateClient'] ]
+__all__ = ['XFlowClient', 'ProcessClient', 'ValueListClient', 'ProcessTemplateClient']
