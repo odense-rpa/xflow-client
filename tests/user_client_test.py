6b40102e4cde0cef31ba8de32e892eaf32b82457
odensesimof <simof@odense.dk>
2025-06-23 12:56:13 +0200

    Fix: user client tests corrected

--- tests/user_client_test.py
+++ tests/user_client_test.py
@@ -2,11 +2,11 @@ from .fixtures import base_client, user_client
 from xflow_client import UserClient
 
 def test_get_user(user_client):
-    user = user_client.get_user("")
-    assert isinstance(user, UserClient)
-    assert user.id == base_client.user_id
+    user = user_client.get_user("03ac653e-edce-43c1-b2a2-4a69b09014a8")
+    assert user is not None
+    assert user["email"] == "simof@odense.dk"
 
 def test_search_users(user_client):
     users = user_client.search_users(email = "simof@odense.dk", cpr = None)
-    assert isinstance(users, list)
-    assert all(isinstance(user, UserClient) for user in users)
+    assert len(users) > 0
+    
