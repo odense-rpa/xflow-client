03474ada95c9b943b08b82aa99a374f914f6b66a
Jakob Kirkegaard Wibroe <jkwibroe91@gmail.com>
2025-06-23 12:18:04 +0200

    typo

--- tests/value_list_client_test.py
+++ tests/value_list_client_test.py
@@ -1,7 +1,7 @@
 from .fixtures import base_client, value_list_client
 from xflow_client import ValueListClient
 
-def test_get_process_templates(value_list_client):     
+def test_search_value_lists(value_list_client):     
     value_list = value_list_client.search_value_lists("RPA")
     assert len(value_list) > 0
 
